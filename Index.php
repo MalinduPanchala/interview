7581f657185eb090f184a285ee083d5eec9fc50e
MalinduPanchala <59525270+MalinduPanchala@users.noreply.github.com>
2021-01-07 12:21:17 +0530

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,35 @@
+<?php 
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST,UPDATE,DELETE');
+
+header("Access-Control-Allow-Headers: Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization");
+
+include('./DBconnection.php');
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "connection success";
+}
+
+$username = $_POST["username"];
+
+$query = "SELECT id,username,password,email from user";
+$array = array();
+
+if ($result = mysqli_query($conn, $query)) {
+    if(mysqli_num_rows($result)>0) {
+        while($row = mysqli_fetch_array($result)) {
+            $array[] = array("id"=>$row["id"],"username"=>$row["username"],"password"=>$row["password"],"email"=>$row["email"]);
+        }
+        echo json_encode($array);
+    } else {
+        echo "items not available";
+    }
+}
+else {
+    echo "Query not executed";
+}
+
+?>
